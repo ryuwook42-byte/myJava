bb5f7a7d2edff4d3542eb4aa7f7b674416e0259a
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-15 02:38:39 +0900

    i work mostly form home

--- src/main/java/Hello6.java
+++ src/main/java/Hello6.java
@@ -3,6 +3,7 @@ public class Hello6 {
     public static void main(String[] args){
 
         System.out.println("영재형 할룽~");
-
+        System.out.print("영재형 할룽`");
+        System.out.print("영재형 바보");
     }
 }
