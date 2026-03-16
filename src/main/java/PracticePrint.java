5a4cd4e60edbd96636c11a7d3932299aca55c427
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-16 15:13:30 +0900

    It is a place to practice based on what you have practiced.

---
+++ src/main/java/PracticePrint.java
@@ -0,0 +1,19 @@
+public class PracticePrint {
+
+    public static void main(String[] args){
+
+        System.out.println("Hellow i'm Exid");
+
+        System.out.println("let me introduce my first project");
+
+        System.out.println("As for this project, I have something to say. However, my English isn't very strong, so please bear whit me if i make any grammar mistakes.");
+
+        System.out.println("Alright, let me introduce myself");
+
+        System.out.print("I'm thirty-three years ago and i born to Republic of Korea in Andong,");
+
+        System.out.println("And i lived on Andong until I was 30");
+
+        System.out.println("I'm studying in the polytech college now");
+    }
+}
