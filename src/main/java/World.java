54acb0cd845d0c7b8ae3ed4e09992c1bcd0845af
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-18 00:27:06 +0900

    It is a place to practice based on what you have practiced.

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,8 +1,8 @@
 public class World {
 
-    public static void name(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("static - 가져다 쓸수 있다");
+        System.out.println("static - 메서드(객체이름)를 공용 도구로 만들어주는 상시도구(객체/Object)라고 생각하면됨");
 
         System.out.println("public - 아무나/누구든지");
 
