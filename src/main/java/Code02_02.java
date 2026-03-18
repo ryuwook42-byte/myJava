da5d6df8cbefd3ebd865c44d781f48970038d0f1
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-18 10:08:29 +0900

    It is a place to practice based on what you have practiced.1

--- src/main/java/Code02_02.java
+++ src/main/java/Code02_02.java
@@ -1,7 +1,6 @@
 public class Code02_02 {
 
     public static void main(String[] args) {
-
         int num1, num2, result;
 
         num1 = 100;
---
+++ src/main/java/Code02_07.java
@@ -0,0 +1,21 @@
+public class Code02_07 {
+
+    public static void main(String[] args) {
+
+        int a, b, c, re;
+        a = 100;
+        b = 50;
+
+        re = a + b;
+        System.out.println(a + "+" + b + "=" + re);
+
+        re = a - b;
+        System.out.println(a + "-" + b + "=" + re);
+
+        re = a * b;
+        System.out.println(a + "*" + b + "=" + re);
+
+        re = a / b;
+        System.out.println(a + "/" + b + "=" + re);
+    }
+}
