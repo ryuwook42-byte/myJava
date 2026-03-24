f3cf94726d087881ce12bb4a397fea1d3e0f608b
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-24 13:02:40 +0900

    It is a place to practice based on what you have practiced.

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -5,7 +5,10 @@ public class Exam01 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("a 값을 입력하세요 ==> ");
         int a = sc.nextInt();
+
+        System.out.print("b 값을 입력하세요 ==> ");
         int b = sc.nextInt();
         double result = (double) a / b;
 
