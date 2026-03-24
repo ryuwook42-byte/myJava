34b83171d9cc20378458a3d4ac177d761d7fc221
ryuwook42-byte <ryuwook42@gmail.com>
2026-03-24 13:45:24 +0900

    It is a place to practice based on what you have practiced.

---
+++ src/main/java/Exam02.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exam02 {
+
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+
+        String fri = "x값을 입력해 주세요. ===> ";
+        String two = "y값을 입력해 주세요. ===> ";
+        String three = "결과 값은 ";
+
+        System.out.print(fri);
+        int x = a.nextInt();
+
+        System.out.print(two);
+        int y = a.nextInt();
+        System.out.print(three);
+
+        double z = (double) x / y;
+
+        System.out.println(x + " 나누기 " + y + "은 " + z + "입니다.");
+
+
+
+
+        a.close();
+    }
+}
